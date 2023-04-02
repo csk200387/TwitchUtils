a3664fbd4b49b82dda884590fb0deaee8a74624b
csk200387 <102679480+csk200387@users.noreply.github.com>
2023-04-02 20:59:05 +0900

    [ADD] onoff noti

--- StreamNotiBot.py
+++ StreamNotiBot.py
@@ -29,31 +29,31 @@ async def is_live(name:str, session) :
                 return True
 
 
-async def send_hook(content:str, session) :
+async def send_hook(content:str, status:bool, session) :
     today = datetime.date.today()
     date = today.strftime("%m월 %d일")
-
+    stat = "뱅온" if status else "뱅종"
     headers = {"Content-Type" : "application/json"}
     data = {
-        "content": f"{date} 뱅온 - {content}",
+        "content": f"{date} {stat} - {content}",
         "username": "StreamLog",
         "avatar_url": "https://i.pinimg.com/474x/40/e4/86/40e486f34c81555ff1b9012aa8a24866.jpg"
         }
     data = json.dumps(data)
-    async with session.post(DISCORD_WEBHOOK_URL, headers=headers, data=data) as response:
+    async with session.post(DISCORD_WEBHOOK_URL, headers=headers, data=data) as response :
         response.raise_for_status()
 
 
-async def main(streamer:str, session) :
+async def main(streamer: str, session) :
     print("Start", streamer)
     status = False
-    while True :
-        try :
+    while True:
+        try:
             now_status = await is_live(streamer, session)
-            if status != now_status and now_status :
+            if status != now_status :
                 status = now_status
-                await send_hook(names[streamer], session)
-        except Exception as e :
+                await send_hook(names[streamer], status, session)
+        except Exception as e:
             print(f"ERROR : {e}, Session : {streamer}")
         await asyncio.sleep(2)
 
